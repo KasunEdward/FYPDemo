d63c1013ecff3cae6cccb71e2d9ff92e40b9ebd3
kasunEdward <kasunedward48@gmail.com>
2017-11-19 14:12:00 +0530

    modified demo.php

--- demo.php
+++ demo.php
@@ -1,25 +1,8 @@
 <?php
 $json = file_get_contents('php://input');
 $obj = json_decode($json);
-// $num =$obj[0]->val1;
-// $array = Array (
-//     "0" => Array (
-//         "id" => "MMZ301",
-//         "name" => "Michael Bruce",
-//         "designation" => "System Architect"
-//     ),
-//     "1" => Array (
-//         "id" => "MMZ385",
-//         "name" => "Jennifer Winters",
-//         "designation" => "Senior Programmer"
-//     ),
-//     "2" => Array (
-//         "id" => "MMZ593",
-//         "name" => "Donna Fox",
-//         "designation" => "Office Manager"
-//     )
-// );
 $json = json_encode($obj);
+
 
 if (file_put_contents("data.json", $json))
     echo "JSON file created successfully...";
